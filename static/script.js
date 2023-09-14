1057e7670cc1b6c42ff1344d84dfdf87cf568d4d
Jekari Rawls <jekarirawls@gmail.com>
2023-09-13 21:07:37 -0400

    added tutorial steps to about page

--- static/script.js
+++ static/script.js
@@ -14,11 +14,41 @@ function myFunction() {
     }
 }
 
-function showStep() {
-    if (step1.style.display === "none") {
-        step1.style.display = "block";
+function showStep1() {
+    let step1 = document.getElementById("step1-more");
+    let dots1 = document.getElementById("dots1");
+    if (dots1.style.display === "none") {
+        dots1.style.display = "block";
+        step1.style.display = "none";
     }
     else {
-        step1.style.display = "none";
+        dots1.style.display = "none";
+        step1.style.display = "block";
+    }
+}
+
+function showStep2() {
+    let step2 = document.getElementById("step2-more");
+    let dots2 = document.getElementById("dots2");
+    if (dots2.style.display === "none") {
+        dots2.style.display = "block";
+        step2.style.display = "none";
+    }
+    else {
+        dots2.style.display = "none";
+        step2.style.display = "block";
+    }
+}
+
+function showStep3() {
+    let step3 = document.getElementById("step3-more");
+    let dots3 = document.getElementById("dots3");
+    if (dots3.style.display === "none") {
+        dots3.style.display = "block";
+        step3.style.display = "none";
+    }
+    else {
+        dots3.style.display = "none";
+        step3.style.display = "block";
     }
 }
