32abba445ea2529d67cd61063d1206ed82a761b3
Jekari Rawls <jekarirawls@gmail.com>
2023-09-21 15:17:15 -0400

    completed search function

--- app.py
+++ app.py
@@ -6,27 +6,30 @@ from flask_session import Session
 from secret_key_generator import secret_key_generator
 from werkzeug.utils import secure_filename
 from decouple import Config
-
+from config import UPLOAD_FOLDER
 
 app = Flask(__name__)
-script_directory = os.path.dirname(os.path.abspath(__file__))
-print(script_directory)
 
+# Configures .env file and gets secret key
 config = Config(".env")
 secret_key = config.get("SECRET_KEY")
 
 
-# Configure session to use filesystem (you can change this to use other storage options)
+# Configure session to use filesystem
 app.config["SESSION_TYPE"] = "filesystem"
 
-# Set a secret key for your application (replace 'your_secret_key' with a secure key)
-# SECRET_KEY = secret_key_generator.generate()
+# Set a secret key for the application
 app.secret_key = secret_key
 
 # Initialize the session extension
 Session(app)
 
+# Configures UPLOAD_FOLDER in config.py
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
+
+# Function that reads .csv file and does tasks
+# Returns dataframe to be sent to renderNetflixData() function
 def getNetflixData(fileName):
     df = pd.read_csv(fileName)
     df = df.drop(
@@ -46,67 +49,78 @@ def getNetflixData(fileName):
     df = df.set_index("Start Time")
     df.index = df.index.tz_convert("US/Eastern")
     df = df.reset_index()
-
     df["Duration"] = pd.to_timedelta(df["Duration"])
-
     return df
 
 
+# Function to render the dataframe in the html table
+# Works for sample file and uploaded file
 def renderNetflixData(filename):
-    df = getNetflixData(filename)
-    return render_template(
-        "data.html",
-        tables=[df.to_html(classes="data-table")],
-        titles=[""],
-    )
+    if filename == "SampleNetflixViewingHistory.csv":
+        file_path = os.path.join("files", filename)
+        df = getNetflixData(file_path)
+        return render_template(
+            "data.html",
+            tables=[df.to_html(classes="data-table")],
+            titles=[""],
+        )
+    else:
+        df = getNetflixData(filename)
+        return render_template(
+            "data.html",
+            tables=[df.to_html(classes="data-table")],
+            titles=[""],
+        )
 
 
+# Renders index.html
 @app.route("/")
 def index():
     return render_template("index.html")
 
 
+# Renders data.html
 @app.route("/data")
 def data():
     return render_template("data.html")
 
 
+# Renders about.html
 @app.route("/about")
 def about():
     return render_template("about.html")
 
 
+# Route to handle form submission for the sample form button
 @app.route("/sample", methods=["POST"])
 def uploadSample():
     if request.method == "POST":
+        session.clear()
         return renderNetflixData("SampleNetflixViewingHistory.csv")
 
 
+# Route to handle form submission for the upload file form button
+# Gets uploaded file name using requests
+# Saves file by securing it and storing it in the proper directory, files
+# Adds uploaded file to the session in the app
 @app.route("/upload", methods=["POST"])
 def upload_file():
     if request.method == "POST":
         uploaded_file = request.files["filename"]
         if uploaded_file.filename != "":
-            # file_path = os.path.join(script_directory, uploaded_file.filename)
-            # uploaded_file.save(file_path)
-            # result = process_file(file_path)
             filename = secure_filename(uploaded_file.filename)
-            uploaded_file.save(os.path.join(script_directory, filename))
-            return renderNetflixData(os.path.join(script_directory, filename))
-        else:
-            flash("Please select a file to upload.")
-            return redirect("/sample")
-        # return renderNetflixData(uploaded_file.filename)
+            uploaded_file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+            session["uploaded_file"] = filename
+            return renderNetflixData(
+                os.path.join(app.config["UPLOAD_FOLDER"], filename)
+            )
 
 
-def process_file(file_path):
-    # Implement your file processing logic here
-    # You can read the file and generate content as needed
-    with open(file_path, "r") as file:
-        file_content = file.read()
-    return file_content
-
-
+# Route to handle the search form submission
+# Gets the search query from the form using reqeust
+# Checks if the uploaded file is in session, if so, search using that file
+# Else, use the sample file
+# Calls a function that filters the file based on the query
 @app.route("/search", methods=["POST"])
 def searchNetflixData():
     if request.method == "POST":
@@ -114,22 +128,18 @@ def searchNetflixData():
         if search_query != "":
             if "uploaded_file" in session:
                 uploaded_filename = session["uploaded_file"]
-                print("this is the file name")
-                print(uploaded_filename)
                 return filterNetflixData(search_query, uploaded_filename)
             else:
                 return filterNetflixData(
                     search_query, "SampleNetflixViewingHistory.csv"
                 )
-        else:
-            flash("Please enter a search query.")
-            return redirect("/sample")
 
 
+# Function that takes in the search query and filters the dataframe based on that query
 def filterNetflixData(search_query, filename):
+    file_path = os.path.join(app.config["UPLOAD_FOLDER"], filename)
     # Load the data from the specified file
-    df = pd.read_csv(filename)
-    search = search_query
+    df = pd.read_csv(file_path)
 
     # Perform necessary data transformations and rendering
     df = df.drop(
@@ -151,7 +161,7 @@ def filterNetflixData(search_query, filename):
     df["Duration"] = pd.to_timedelta(df["Duration"])
 
     # Filter the DataFrame based on the search_query
-    filtered_df = df[df["Title"].str.contains(search, regex=False)]
+    filtered_df = df[df["Title"].str.contains(search_query, regex=False)]
     if filtered_df.empty:
         flash("No matching entries found.")
         return redirect("/sample")
@@ -163,5 +173,6 @@ def filterNetflixData(search_query, filename):
     )
 
 
+# App debugging
 if __name__ == "__main__":
     app.run(debug=True)
